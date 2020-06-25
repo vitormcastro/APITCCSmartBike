c98ebf7b6fb857462293384315c4c12b7b4d702b
Vítor Marques de Castro <vitormcastro@outlook.com>
2020-06-25 18:38:27 -0300

    Add de novos models

---
+++ APITCCSmartBike/Models/DadosPorPeriodoModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APITCCSmartBike.Models
+{
+    public class DadosPorPeriodoModel
+    {
+        [Required]
+        public string IdBike { get; set; }
+
+        [Required]
+
+        public string DeDataHora { get; set; }
+
+
+        public string AteDataHora { get; set; }
+
+        public string Type { get; set; }
+    }
+}
---
+++ APITCCSmartBike/Models/UserModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APITCCSmartBike.Models
+{
+    public class UserModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Genero { get; set; }
+
+        [Required]
+        public string Nascimento { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string ImgBase64 { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    
+    }
+}
