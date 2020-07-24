eae7114728c56205e29db1ffacfebee85944ef83
Vítor Marques de Castro <vitormcastro@outlook.com>
2020-07-23 21:13:56 -0300

    Corrigi o get user all

--- APITCCSmartBike/Controllers/UserController.cs
+++ APITCCSmartBike/Controllers/UserController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using APITCCSmartBike.Interfaces;
+
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -21,28 +23,43 @@ namespace APITCCSmartBike.Models
             _userService = userService;
         }
 
-        [HttpGet ("getall")]
+        [HttpGet("getall")]
         public IActionResult GetAll()
         {
+            string response = string.Empty;
             try
-            {               
-                var users = _userService.GetAll();
-          
-                return Ok(users);
-            }
-            catch(Exception err)
             {
-                return BadRequest(err);
+                var users = _userService.GetAll(ref response);
+                if (users == null)
+                {
+                    return BadRequest(new
+                    {
+                        status = "Failed",
+                        message = response
+                    });
+                }
+                return Ok(new
+                {
+                    status = response,
+                    users
+                });
             }
-          
+            catch (Exception err)
+            {
+                return BadRequest(new
+                {
+                    status = "Failed",
+                    message = err.Message
+                });
+            }
         }
 
         [AllowAnonymous]
         [HttpPost("create")]
-        public IActionResult CreateUser([FromBody]UserModel model)
+        public IActionResult CreateUser([FromBody] UserModel model)
         {
             string responde = string.Empty;
-            if(_userService.CreateUser(model,ref responde))
+            if (_userService.CreateUser(model, ref responde))
             {
                 return Ok(new
                 {
